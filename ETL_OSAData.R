60fbaa09b2d554ec7d7b8fc33314fd26702fa6fb
beaesteban01 <bea1596@gmail.com>
2020-10-06 13:38:09 +0200

    Funcional para eda y feature transformation

--- ETL_OSAData.R
+++ ETL_OSAData.R
@@ -7,15 +7,43 @@
 # Clear the working space
 rm(list=ls())
 
+
+library(readxl) # Using readxl package to read an Excel file
+library(naniar)
+# You can work with dplyr
+# for using select(df_tmp, Patient, Gender,...)
+# https://cran.r-project.org/web/packages/dplyr/vignettes/dplyr.html
+# dplyr's basic set of tools to apply on data frames
+
+library(dplyr) # asi importamos librerias --> tipo import en python
+#dplyr is a basic package for R tipo numpy, pandas etc para python
+#data.table libreria de R que se usa para trabajar con datasets muy grandes
+
+library(visdat) #visualize 
+# You can use tidyr
+# https://blog.rstudio.com/2014/07/22/introducing-tidyr/
+# tidyr is a package that makes it easy to "tidy" 
+# your data.
+#
+# Tidy data is data that's easy to work with:
+# it's easy to munge (with dplyr), visualise (with ggplot2 or ggvis) and model (with R's hundreds of modelling packages). The two most important properties of tidy data are:
+#
+# Each column is a variable.
+#
+# Each row is an observation.
+
+library(tidyr)
+# Write the clean data into Output_file
+# you can install writexl package
+
+library(writexl)
+library("RColorBrewer") #For colors
+
 Input_file <- "Info_BDApnea_QuironMalaga.xlsx"
 Output_file <- "OSA_DB_UPM.xlsx"
 
 Data_Directory <- "/Users/beatrizesteban/OneDrive - Universidad Politécnica de Madrid/Segundo MUIT/PRDL&MLLB/OSA/DATA/"
 
-# Using readxl package to read an Excel file
-# Install the readxl package is nor already installed
-
-library(readxl)
 
 # paste --> concatenation of to strings
 df_tmp <- read_excel(paste(Data_Directory, Input_file, sep = ""))
@@ -35,16 +63,6 @@ class(df_tmp)
 # to work with:
 # Patient, Gender, IAH, Peso, Talla, Edad, PerCervical
 
-# You can work with dplyr
-# for using select(df_tmp, Patient, Gender,...)
-# https://cran.r-project.org/web/packages/dplyr/vignettes/dplyr.html
-# dplyr's basic set of tools to apply on data frames
-
-library(dplyr) # asi importamos librerias --> tipo import en python
-#dplyr is a basic package for R tipo numpy, pandas etc para python
-#data.table libreria de R que se usa para trabajar con datasets muy grandes
-
-
 df_tmp1 <- select(df_tmp, Patient, Gender, IAH, Peso, Talla, Edad, PerCervical, Fumador, Roncador)
 df_tmp1 <- df_tmp1 %>% rename(Weight = Peso,
                                 Height = Talla,
@@ -53,106 +71,65 @@ df_tmp1 <- df_tmp1 %>% rename(Weight = Peso,
                                 Smoker = Fumador, 
                                 Snorer = Roncador)
 
-######## HOW TO MANAGE Non Available values !!!
-# To change -1 values for NA in all columns (i.e. variables)
-# you can install package naniar
-# load it
-#    note: you can also see
-#          https://cran.r-project.org/web/packages/naniar/vignettes/getting-started-w-naniar.html
-
-
-# BUT first you can visualize the issue with chr, num and NA
-# using visdat
-
-#en tmp1 podemos ver valores que nos vana  dar problemas como negativos
-# o huecos ej: column peso --> algunos text values
-
-library(visdat) 
-vis_dat(df_tmp1) # vemos un plot con que tipo de valores tenemos en el df
-
-
-# BEFORE CONTINUING EXECUTING NEXT lines of code
-#        BE SURE TO FIX the Excel removing comments in Peso --> WTFFFFFF 
- # segun el borrarlo dele xcel, crear otro nuevo y cambiar el input al ppo del script 
-
-##### QUESTIONS: #############################################
-
-### HOW MANY PATIENTS?
-
-### HOW MANY MALE /FEMALE
-
-
-
-library(naniar)
-
+# EXPLORATORY DATA ANALYSIS
+# See what kind of data we have in the df
+vis_dat(df_tmp1) 
 str(df_tmp1)
-#df_tmp1_peso <- select(df_tmp1, Peso)
 
-#df_tmp1_peso <- select(df_tmp, Patient, Gender, IAH, Peso, Talla, Edad, PerCervical)
+color <- brewer.pal(7, "Set3") 
+#length nos da el value contando los NA, en realidad es -1 de eso 
 
-# Convert Peso column from character type to numeric type
-# String values are converted to NA 
-df_tmp1$Weight = as.numeric(df_tmp1$Weight)
+length(unique(df_tmp1$Gender)) # 2 diff. values
+barplot(table(df_tmp1$Gender), ylim = c(0,550), main= "Genders", col=color)
 
+length(unique(df_tmp1$Smoker)) # 6 diff. values
+barplot(table(df_tmp1$Smoker), ylim = c(0,550),main= "Smokers", col=color)
+
+length(unique(df_tmp1$Snorer)) # 8 diff. values
+barplot(table(df_tmp1$Snorer), ylim = c(0,550),main= "Snorer", col=color)
+
+length(unique(df_tmp1$Patient)) # 684 diff. values
+length(unique(df_tmp1$Weight)) # 92 diff. values
+
+
+
+# FEATURES TRANSFORMATIONS
+
+# Drop Patient feature
+df_tmp1 <- subset(df_tmp1, select = -Patient)
+
+# Factor and numeric conversion 
 df_tmp1$Gender = factor(df_tmp1$Gender)
 df_tmp1$Gender = as.numeric(df_tmp1$Gender)
 
 df_tmp1$Smoker = factor(df_tmp1$Smoker)
 df_tmp1$Smoker = as.numeric(df_tmp1$Smoker)
 
+#plot(table(df_tmp1$Smoker))
 df_tmp1$Snorer = factor(df_tmp1$Snorer)
 df_tmp1$Snorer= as.numeric(df_tmp1$Snorer)
 
+# Numeric conversion
+# String values replace with NA
+df_tmp1$Weight = as.numeric(df_tmp1$Weight)
 
-library(visdat) 
 vis_dat(df_tmp1)
 
 # Now change -1 values for NA in all columns (i.e. variables)
 df_tmp2 <- replace_with_na_all(df_tmp1,condition = ~.x == -1)
+
 vis_dat(df_tmp2)
+
 # Finally remove (drop out) all rows containing a NA
 # at least one column
 
-# You can use tidyr
-# https://blog.rstudio.com/2014/07/22/introducing-tidyr/
-# tidyr is a package that makes it easy to "tidy" 
-# your data.
-#
-# Tidy data is data that's easy to work with:
-# it's easy to munge (with dplyr), visualise (with ggplot2 or ggvis) and model (with R's hundreds of modelling packages). The two most important properties of tidy data are:
-#
-# Each column is a variable.
-#
-# Each row is an observation.
-
-library(tidyr)
-
 df_final <- df_tmp2 %>% drop_na()
 # %>% para aplicar una operacion sobre el df
-##### QUESTIONS: #############################################
-#####   AFTER CLEANING:
-#####
-#####       HOW MANY PATIENTS?
-#####       HOW MANY MALE /FEMALE?
 
-#library(visdat) 
 vis_dat(df_final) 
 str(df_final) #REcompruebo que menos patient y gender es numeric
 
 ######### SAVING CLEAN DATA ##################################
-# Write the clean data into Output_file
-# you can install writexl package
-
-library(writexl)
-
-# You can change the names of the columns
-
-df_final <- df_final %>% rename(Weight = Peso,
-                                Height = Talla,
-                                Age = Edad,
-                                Cervical = PerCervical) 
-
-vis_dat(df_final)
 
 write_xlsx(df_final,
            paste(Data_Directory, Output_file, sep = ""))
