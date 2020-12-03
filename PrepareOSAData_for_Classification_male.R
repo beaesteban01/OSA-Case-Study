584f3220debe397bfc8995747444c318fa9e4135
beaesteban01 <bea1596@gmail.com>
2020-12-03 17:08:45 +0100

    Preparacion del dataset con 3 clases y con dos, con R

--- PrepareOSAData_for_Classification_male.R
+++ PrepareOSAData_for_Classification_male.R
@@ -8,7 +8,8 @@
 
 rm(list=ls())
 Input_file <- "OSA_DB_UPM.xlsx"
-Output_file <- "OSA_extreme_male.xlsx"
+Output_file <- "OSA_class.xlsx"
+Output_file_bin <- "OSA_class_bin.xlsx"
 Data_Directory <- "/Users/beatrizesteban/OneDrive - Universidad Politécnica de Madrid/Segundo MUIT/PRDL&MLLB/OSA/DATA/"
 
 
@@ -26,7 +27,7 @@ dim(df_OSA)
 
 ## We will begin considering male subjets
 ### Male population
-df_OSA_male=subset(df_OSA, Gender==1)
+#df_OSA_male=subset(df_OSA, Gender==1)
 
 
 ########################################
@@ -38,29 +39,35 @@ df_OSA_male=subset(df_OSA, Gender==1)
 # We will use dplyr library (mutate operator)
 library(dplyr)
 
-df_OSA_male <- df_OSA_male %>%
+#3 classes
+df_OSA <- df_OSA %>%
           mutate(OSA = ifelse(IAH <= 10, "Healthy",
           ifelse(IAH>=30, "Severe", "Mild")))
-
-df_OSA_male <- df_OSA_male %>% filter(OSA != "Mild")
+# 2 clasess
+df_OSA_bin <- df_OSA %>% filter(OSA != "Mild")
 
 
 # Define OSA as a factor!
   
-df_OSA_male$OSA = factor(df_OSA_male$OSA)
+df_OSA$OSA = factor(df_OSA$OSA)
+df_OSA_bin$OSA = factor(df_OSA_bin$OSA)
 
 # Add BMI column
-df_OSA_male$BMI <-
-  with(df_OSA_male, Weight / (Height/100.0)^2)
+df_OSA$BMI <-
+  with(df_OSA, Weight / (Height/100.0)^2)
+df_OSA_bin$BMI <-
+  with(df_OSA_bin, Weight / (Height/100.0)^2)
 
-summary(df_OSA_male)
+summary(df_OSA_bin)
+summary(df_OSA)
+
 
 ###############################################
 ### READ MORE: On using R as SQL
 ###
 # https://blog.exploratory.io/using-dplyr-to-query-databases-directly-instead-of-using-sql-fed43f059ed6
 
-df_OSA_male %>% group_by(OSA) %>% summarise(Ave_IAH = mean(IAH))
+#df_OSA_male %>% group_by(OSA) %>% summarise(Ave_IAH = mean(IAH))
 
 ###### SAVE the data frame into an EXCEL file
 #
@@ -69,7 +76,10 @@ df_OSA_male %>% group_by(OSA) %>% summarise(Ave_IAH = mean(IAH))
 #
 
 library(writexl)
-
-write_xlsx(df_OSA_male,
+#guardo el de 3 clases
+write_xlsx(df_OSA,
            paste(Data_Directory, Output_file, sep = ""))
+#guardo el de 2 clases
+write_xlsx(df_OSA,
+           paste(Data_Directory, Output_file_bin, sep = ""))
 
